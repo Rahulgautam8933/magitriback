1ab50d133930f0951381f2ab3578678b51a11e18
unknown <rahul>
2023-05-08 15:09:14 +0530

    change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const User = require('./model/userScema');
 app.use(require('./router/ruth'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 
 app.listen(PORT,()=>{
